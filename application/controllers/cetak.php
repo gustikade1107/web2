f44d344fc07ec6ea0f58fd18b3efc7e7967214d4
gustikade1107 <ratubagus01@gmail.com>
2020-10-27 23:05:22 -0700

    praktikum bootstrap 27/10

---
+++ application/controllers/cetak.php
@@ -0,0 +1,14 @@
+<?php
+
+class cetak extends CI_Controller {
+	public function index($url_nim='',$url_jur='')
+	{
+		$data['nama'] = 'Raditha';
+		$data['umur'] = '17';
+		$data['nim'] = $url_nim;
+		$data['jurusan'] = $url_jur;
+
+	$this->load->view('mahasiswa/v_index', $data);
+
+	}
+}
